41882dd8d0e2f4adaef17a8fde927095a9c1215a
unknown <matiaskern22@gmail.com>
2026-04-07 14:25:41 -0300

    clase 4 4to

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from './components/HeaderComponent.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <HeaderComponent />
 </template>
---
+++ vite-project/src/components/HeaderComponent.vue
@@ -0,0 +1,24 @@
+<script>
+
+</script>
+
+<template>
+<Header>
+    <div>
+        <a href="#" aria-label="Bananas y Platanos, inicio">
+            <span aria-hidden="true">🐒</span>
+            <span>Bananas y Platanos</span>
+            <span>Alimentos para monos</span>
+        </a>
+        <nav>
+            <ul>
+                <li>Acá va a estar el nav</li>
+            </ul>
+        </nav>
+    </div>
+</Header>
+</template>
+
+<style>
+
+</style>
